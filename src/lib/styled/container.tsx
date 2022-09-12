7fe8fed6fabde9d34aebec668d716a329c59e5a6
stelsp <stelspeh@gmail.com>
2022-09-12 19:14:10 +0300

    fix: container description

--- src/lib/styled/container.tsx
+++ src/lib/styled/container.tsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  // не может быть больше (--max-width)
+  // если ширина экрана меньше (--max-width),
+  // то ширина 100% - (--space) по сторонам
+
   --max-width: 1200px;
   --space: 16px;
 

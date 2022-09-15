bd5c1b4ebc943ca8d51f52761d9105390f351605
stelsp <stelspeh@gmail.com>
2022-09-15 23:23:44 +0300

    feat: add basic layout

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,12 +1,55 @@
 import React, { FC } from 'react';
 import { Container } from '../../lib/styled/container';
-import { Section } from './style';
+import { Card, Grid, Section } from './style';
+
+const mockCards = [
+  {
+    project: 'project',
+    descr: 'descr',
+    id: 1
+  },
+  {
+    project: 'project',
+    descr: 'descr',
+    id: 2
+  },
+  {
+    project: 'project',
+    descr: 'descr',
+    id: 3
+  },
+  {
+    project: 'project',
+    descr: 'descr',
+    id: 4
+  }
+];
+
+interface ProjectProps {
+  project: string;
+  descr: string;
+  id: number;
+}
+
+const Project: FC<ProjectProps> = ({ project, descr, id }) => {
+  return (
+    <Card>
+      <div>{project}</div>
+      <div>{descr}</div>
+      <div>{id}</div>
+    </Card>
+  );
+};
 
 const Projects: FC = () => {
   return (
     <Section>
       <Container>
-        <p>Projects</p>
+        <Grid>
+          {mockCards.map((card) => (
+            <Project project={card.project} descr={card.descr} id={card.id} key={card.id} />
+          ))}
+        </Grid>
       </Container>
     </Section>
   );
--- src/components/Projects/style.tsx
+++ src/components/Projects/style.tsx
@@ -3,3 +3,14 @@ import styled from 'styled-components';
 export const Section = styled.section`
   background-color: var(--color-white);
 `;
+
+export const Grid = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  justify-items: center;
+`;
+
+export const Card = styled.li`
+  display: flex;
+  flex-direction: column;
+`;

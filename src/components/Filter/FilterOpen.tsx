ed5009f361d768824e9baee7d00e4c811dd9bed7
achemakin <kotor79@gmail.com>
2022-09-23 01:49:22 +0300

    feat: Dropdown, FilterOpen

--- src/components/Filter/FilterOpen.tsx
+++ src/components/Filter/FilterOpen.tsx
@@ -1,19 +1,119 @@
 import React, { FC } from 'react';
-import { Form, FormName, FormWrapper, FormButton, Submit, Dropdown } from './styleFilterOpen';
+import { Form, FormName, FormWrapper, Submit } from './styleFilterOpen';
+import Dropdown from './Dropdown';
 
 const FilterOpen: FC = () => {
   const selects = [
     {
       id: 0,
-      title: 'Направление'
+      title: 'Направление',
+      type: 'checkbox',
+      list: [
+        {
+          name: 'direction[]',
+          value: 'Backend',
+          val: false
+        },
+        {
+          name: 'direction[]',
+          value: 'Frontend',
+          val: false
+        },
+        {
+          name: 'direction[]',
+          value: 'UI/UX',
+          val: false
+        },
+        {
+          name: 'direction[]',
+          value: 'Тестировщик ПО',
+          val: false
+        },
+        {
+          name: 'direction[]',
+          value: 'Project-менеджер',
+          val: false
+        },
+        {
+          name: 'direction[]',
+          value: 'Контроллёр',
+          val: false
+        },
+        {
+          name: 'direction[]',
+          value: 'Data Scientist',
+          val: false
+        }
+      ]
     },
     {
       id: 1,
-      title: 'Стек'
+      title: 'Стек',
+      type: 'checkbox',
+      list: [
+        {
+          name: 'stack[]',
+          value: 'Python',
+          val: false
+        },
+        {
+          name: 'stack[]',
+          value: 'FastApi',
+          val: false
+        },
+        {
+          name: 'stack[]',
+          value: 'PostgreSQL',
+          val: false
+        },
+        {
+          name: 'stack[]',
+          value: 'Тестировщик ПО',
+          val: false
+        },
+        {
+          name: 'stack[]',
+          value: 'pydantic',
+          val: false
+        }
+      ]
     },
     {
       id: 2,
-      title: 'Дата'
+      title: 'Дата',
+      type: 'radio',
+      list: [
+        {
+          name: 'date',
+          value: 'За всё время',
+          val: false
+        },
+        {
+          name: 'date',
+          value: 'За последний день',
+          val: false
+        },
+        {
+          name: 'date',
+          value: 'За последние 3 дня',
+          val: false
+        },
+        {
+          name: 'date',
+          value: 'За последнюю неделю',
+          val: false
+        },
+        {
+          name: 'date',
+          value: 'За последний месяц',
+          val: false
+        },
+        {
+          name: 'date',
+          value: 'За последние полгода',
+          val: false
+        }
+      ]
     }
   ];
 
@@ -25,14 +125,7 @@ const FilterOpen: FC = () => {
 
       <FormWrapper>
         {selects.map((item) => (
-          <FormButton
-            type="button"
-            key={item.id}
-            className={currentSelect === item.id ? 'open' : ''}
-            onClick={() => setCurrentSelect(item.id)}
-          >
-            {item.title}
-          </FormButton>
+          <Dropdown item={{ ...item, currentSelect, setCurrentSelect }} key={item.id} />
         ))}
 
         <Submit onClick={(e) => e.preventDefault()}>Показать</Submit>

277cf68f9432579d1c6046928b14a6ff86fe3937
Aleksey Popov <alekseypopov.dev@gmail.com>
2022-09-26 08:39:20 +0300

    fix: refactoring Footer

--- src/layout/Footer/style.tsx
+++ src/layout/Footer/style.tsx
@@ -6,46 +6,48 @@ export const StyledFooter = styled.footer`
 `;
 
 export const Wrapper = styled.div`
-  position: relative;
+  position: static;
+  flex-direction: column;
   display: flex;
   justify-content: space-between;
   align-items: center;
   padding: 31px 0;
 
-  @media (max-width: 320px) {
-    position: static;
-    flex-direction: column;
+  @media (min-width: 1024px) {
+    position: relative;
+    flex-direction: row;
   }
 `;
 
 export const Social = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
-  gap: 25px;
-  height: 18px;
+  gap: 16px;
+  height: 100%;
   z-index: 2;
 
-  @media (max-width: 320px) {
-    flex-direction: column;
-    gap: 16px;
-    height: 100%;
+  &:not(:last-child) {
+    margin: 22px 0 16px 0;
+  }
 
-    &:not(:last-child) {
-      margin: 22px 0 16px 0;
-    }
+  @media (min-width: 1024px) {
+    flex-direction: row;
+    gap: 25px;
+    height: 18px;
   }
 `;
 
 export const FooterLink = styled(Link)`
-  padding: 0 25px 0 0;
-  border-right: 1px solid var(--color-black);
+  padding: 0;
+  border: none;
   font-weight: var(--fw-300);
   font-size: var(--fs-300);
   line-height: var(--line-height-300);
 
-  @media (max-width: 320px) {
-    padding: 0;
-    border: none;
+  @media (min-width: 1024px) {
+    padding: 0 25px 0 0;
+    border-right: 1px solid var(--color-black);
   }
 `;
 
@@ -57,20 +59,6 @@ export const FooterText = styled.span`
 
 export const Logo = styled.p`
   position: absolute;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  font-size: var(--fs-700);
-  font-weight: var(--fw-700);
-  line-height: var(--line-height-700);
-
-  @media (max-width: 320px) {
-    display: none;
-  }
-`;
-
-export const LogoMobile = styled.p`
   display: none;
   align-items: center;
   justify-content: center;
@@ -79,8 +67,22 @@ export const LogoMobile = styled.p`
   font-weight: var(--fw-700);
   line-height: var(--line-height-700);
 
-  @media (max-width: 320px) {
+  @media (min-width: 1024px) {
     display: flex;
+  }
+`;
+
+export const LogoMobile = styled.p`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  font-size: var(--fs-700);
+  font-weight: var(--fw-700);
+  line-height: var(--line-height-700);
+
+  @media (min-width: 1024px) {
+    display: none;
   }
 `;
 
@@ -96,12 +98,12 @@ export const SocialLink = styled.a`
   line-height: var(--line-height-300);
 
   &:not(:last-child) {
-    margin: 0 25px 0 0;
+    margin: 0 16px 0 0;
   }
 
-  @media (max-width: 320px) {
+  @media (min-width: 1024px) {
     &:not(:last-child) {
-      margin: 0 16px 0 0;
+      margin: 0 25px 0 0;
     }
   }
 `;
@@ -109,8 +111,9 @@ export const SocialLink = styled.a`
 export const IconDiv = styled.div`
   display: flex;
   flex-direction: row;
+  margin: 8px 0 0 0;
 
-  @media (max-width: 320px) {
-    margin: 8px 0 0 0;
+  @media (min-width: 1024px) {
+    margin: 0;
   }
 `;

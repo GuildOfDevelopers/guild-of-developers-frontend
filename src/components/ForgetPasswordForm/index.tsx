0624715f57d02f44e2271fb95014177fd87a6034
stelsp <stelspeh@gmail.com>
2022-10-27 21:57:07 +0300

    feat: forgetPassword form + page

---
+++ src/components/ForgetPasswordForm/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import { Container } from '../../lib/styled/container';
+import { Section, Wrapper } from './style';
+
+const ForgetPasswordForm: FC = () => {
+  return (
+    <Section>
+      <Container>
+        <Wrapper>ForgetPasswordForm</Wrapper>
+      </Container>
+    </Section>
+  );
+};
+
+export default ForgetPasswordForm;
